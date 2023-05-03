3a6dc694a1fd7baef562bfeeb0092c7d4ebc28f6
wjdtn0829 <oh050211@gmail.com>
2023-05-03 10:53:17 +0900

    feat:: setting

---
+++ src/main/java/com/example/talktopi/TalkTopiApplication.java
@@ -0,0 +1,13 @@
+package com.example.talktopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkTopiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkTopiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/talktopi/TalkTopiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talktopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkTopiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
