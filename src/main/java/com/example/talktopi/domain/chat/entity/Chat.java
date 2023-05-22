90bda91cbad169c631cc48705264c3debc5ea353
wheeeung <whee050916@gmail.com>
2023-05-22 11:16:49 +0900

    feat:: edit collection name

--- src/main/java/com/example/talktopi/domain/chat/entity/Chat.java
+++ src/main/java/com/example/talktopi/domain/chat/entity/Chat.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Data
-@Document(collation = "TalkTopi")
+@Document(collation = "chat")
 public class Chat {
     @Id
     private String id;
