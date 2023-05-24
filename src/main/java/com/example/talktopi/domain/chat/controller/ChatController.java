531908658c092e42bd650e11b58de63307e78fff
wheeeung <whee050916@gmail.com>
2023-05-24 11:12:02 +0900

    feat:: add chat controller

--- src/main/java/com/example/talktopi/domain/chat/controller/ChatController.java
+++ src/main/java/com/example/talktopi/domain/chat/controller/ChatController.java
@@ -3,10 +3,11 @@ package com.example.talktopi.domain.chat.controller;
 import com.example.talktopi.domain.chat.entity.Chat;
 import com.example.talktopi.domain.chat.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.time.LocalDateTime;
 
@@ -19,5 +20,19 @@ public class ChatController {
     public Mono<Chat> sendMessage(@RequestBody Chat chat) {
         chat.setCreatedAt(LocalDateTime.now());
         return chatRepository.save(chat);
+    }
+
+    @CrossOrigin
+    @GetMapping(value="/sender/{sender}/receiver/{receiver}",produces= MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable int sender, @PathVariable int receiver){
+        return chatRepository.mFindBySender(sender,receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/room/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getRoomMsg(@PathVariable int roomNum) {
+        return chatRepository.mFindByRoomNum(roomNum)
+                .subscribeOn(Schedulers.boundedElastic());
     }
 }
