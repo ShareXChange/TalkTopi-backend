ba54837dc8ad9f8b2d43435189140d72ece4de00
오정수 <112403916+wjdtn0829@users.noreply.github.com>
2023-06-02 14:01:42 +0900

    Delete CommentRepository.java

--- src/main/java/com/example/talktopi/domain/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.talktopi.domain.comment.repository;
-
-import com.example.talktopi.domain.comment.entity.Comment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentRepository extends CrudRepository<Comment, Integer> {
-}
